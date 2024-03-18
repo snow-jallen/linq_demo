aae142c1bd9a5a0dbc58a3b7ef23a2d31a45f596
Jonathan Allen <jallen@snow.edu>
2024-03-18 08:26:34 -0600

    include country and postal code

--- LinqDemo.Persistence/SqliteDataProvider.cs
+++ LinqDemo.Persistence/SqliteDataProvider.cs
@@ -25,13 +25,15 @@ public class SqliteDataProvider : IDataProvider
 [Table("customers")]
 public class Customer
 {
-    public int CustomerId{get;set;}
-    public string FirstName{get;set;}
-    public string LastName{get;set;}
-    public string Company{get;set;}
-    public string Address{get;set;}
-    public string City{get;set;}
-    public string State{get;set;}
-    public string Phone{get;set;}
-    public string Email{get;set;}
+    public int CustomerId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Company { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+    public string PostalCode { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
 }
